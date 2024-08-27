c3d710e45c43a804a78022f4746efabc2e0dbb6d
Sergey Zmitrukevich <sergeyzmitrukevich@yandex.ru>
2024-08-28 02:24:21 +0300

    add Yandex.Metrika 2

--- pages/_document.js
+++ pages/_document.js
@@ -37,7 +37,7 @@ export default class MyDocument extends Document {
                         type="text/javascript"
                         dangerouslySetInnerHTML={{
                             __html: `
-                (function(m,e,t,r,i,k,a){m[i]=m[i]||[]).push(arguments);
+                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
                 m[i].l=1*new Date();
                 for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
                 k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
